edaf0b09c4ed2bc6769ce27969683aded5963819
yasin <yasinleng@yeah.net>
2020-08-24 17:09:09 +0800

    feat：添加卡顿分析

---
+++ app/src/main/java/com/yasin/explore/App.java
@@ -0,0 +1,32 @@
+package com.yasin.explore;
+
+import android.app.Application;
+import android.content.Context;
+import android.util.Log;
+
+import com.github.moduth.blockcanary.BlockCanary;
+import com.github.moduth.blockcanary.BlockCanaryContext;
+import com.github.moduth.blockcanary.internal.BlockInfo;
+
+/**
+ * create by yasin
+ * email yasinleng@163.com
+ */
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        BlockCanary.install(this,new BlockCanaryContext(){
+            @Override
+            public int provideBlockThreshold() {
+                return 160;
+            }
+
+            @Override
+            public void onBlock(Context context, BlockInfo blockInfo) {
+                Log.d("id1234", "卡顿 blockInfo = [" + blockInfo + "]");
+            }
+        }).start();
+    }
+}
