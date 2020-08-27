f914e4d9d1e057ba8be6e3b676f685fe16596994
yasin <yasinleng@yeah.net>
2020-08-27 18:07:43 +0800

    feat：添加图片地址

--- app/src/main/java/com/yasin/explore/MainActivity.java
+++ app/src/main/java/com/yasin/explore/MainActivity.java
@@ -6,6 +6,12 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+//    I/id1234: https://t-youpin.quanhoo.cn/txcloud/biz/after/20200806/55295777-5c63-4d96-9b17-7ecd3d7c43da4456373922110971020.jpg
+//    I/id1234: https://t-youpin.quanhoo.cn/txcloud/biz/after/20200813/0e9d885c-d6a7-4211-b315-c372d15c7f726758572739270479401.jpg
+//    I/id1234: https://t-youpin.quanhoo.cn/txcloud/biz/after/20200729/147bcc66-6a42-400b-a2c2-5a3bb2ff9ebd5937440132523899808.jpg
+//    I/id1234: https://t-youpin.quanhoo.cn/txcloud/biz/after/20200811/a2383725-9571-4878-9b60-ca7de13c183a3697904494515354447.jpeg
+//    I/id1234: https://t-youpin.quanhoo.cn/txcloud/biz/after/20200811/4d365312-832b-4c21-8b8a-863afc16cf034978921428327752158.jpeg
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
